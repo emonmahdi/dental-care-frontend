50a3949927bd232b389aee3497dba11fbb93a3aa
emonmahdi <developeremon1997@gmail.com>
2025-05-02 21:12:47 +0600

    folder structure done and home setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h2 className="text-3xl font-bold underline bg-pink-900 text-white p-4">
-        Dental Care Application
-      </h2>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <h2>Main Content</h2>
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
