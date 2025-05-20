ed8c8e6577c65c5f430afb46b74e29da85cc8c6e
emonmahdi <developeremon1997@gmail.com>
2025-05-20 19:52:21 +0600

    register form hook form use validation done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { Link } from "react-router";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center px-4 bg-gray-100">
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
         <h2 className="text-xl font-bold text-center mb-4">Sign Up</h2>
-        <form className="space-y-5">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
           <div>
             <label className="block mb-1 text-gray-700 font-medium text-[14px]">
               Name
             </label>
             <input
               type="text"
+              {...register("name", { required: true })}
               placeholder="Enter your Name"
               className="w-full px-4 py-2 border border-[#CFCFCF] rounded-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
             />
+            {errors.name && (
+              <span className="text-red-400">Name is required</span>
+            )}
           </div>
           <div>
             <label className="block mb-1 text-gray-700 font-medium text-[14px]">
@@ -23,9 +38,13 @@ const Register = () => {
             </label>
             <input
               type="email"
+              {...register("email", { required: true })}
               placeholder="Enter your email"
               className="w-full px-4 py-2 border border-[#CFCFCF] rounded-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
             />
+            {errors.email && (
+              <span className="text-red-400">Email is required</span>
+            )}
           </div>
           <div>
             <label className="block mb-1 text-gray-700 font-medium text-[14px]">
@@ -33,9 +52,13 @@ const Register = () => {
             </label>
             <input
               type="password"
+              {...register("password", { required: true })}
               placeholder="Enter your password"
               className="w-full px-4 py-2 border border-[#CFCFCF] rounded-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
             />
+            {errors.password && (
+              <span className="text-red-400">Email is required</span>
+            )}
             <div className="text-right mt-1">
               <a href="#" className="text-sm text-secondary hover:underline">
                 Forgot Password?
